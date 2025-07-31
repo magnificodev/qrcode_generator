0b40c07740b7330a7620188cf68bb33a9c90ba2a
magnificodev <richardnguyen26th8@gmail.com>
2025-07-31 11:02:28 +0700

    Complete QRCode Generator

---
+++ qrcode-generator.py
@@ -0,0 +1,29 @@
+import os
+import qrcode
+
+domain = "https://sample-domain.com"
+start_uid = 1
+end_uid = 500
+
+os.makedirs("qr_codes", exist_ok=True)
+
+for i in range(start_uid, end_uid + 1):
+    
+    uid = f"{i:03d}" # Format the uid to be 3 digits long
+    url = f"{domain}?uid={uid}" # Create the url with the uid
+    
+    # Create the QR code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url) # Add the url to the QR code
+    qr.make(fit=True) # Make the QR code fit the data
+
+    img = qr.make_image(fill_color="black", back_color="white") # Create the image
+    img.save(f"qr_codes/{uid}.png") # Save the image
+    print(f"Generated QR code for {url} --> qr_codes/{uid}.png")
+
+print("All QR codes generated successfully.")
